f53a15c7bcd2db6c9b098b7b93598027ecd4f2c4
Ben Scott <btscott@amazon.com>
2026-06-19 11:43:50 +0000

    Fix omz creating new shell

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@ set -euxo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-## Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-
-omz plugin enable aliases aws fzf ssh thefuck vi-mode
-
 # Install homebrew
 echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -17,15 +12,15 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 # Install brew packages
 brew install nvim fzf tmux ripgrep mise direnv zsh-autosuggestions zsh-syntax-highlighting
 
+# Install oh-my-zsh (don't let it launch a new shell)
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
 # Symlink configs
 mkdir -p ~/.config/mise
 ln -s "$SCRIPT_DIR/nvim" ~/.config/nvim
 ln -s "$SCRIPT_DIR/tmux/.tmux.conf" ~/.tmux.conf
 ln -s "$SCRIPT_DIR/mise/config.toml" ~/.config/mise/config.toml
 ln -s "$SCRIPT_DIR/.zshrc.local" ~/.zshrc.local
-
-# Source custom config from omz's .zshrc
-echo 'source ~/.zshrc.local' >> ~/.zshrc
 
 # Symlink scripts to ~/bin
 mkdir -p ~/bin
@@ -35,3 +30,9 @@ done
 
 # TMUX plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Configure zsh
+sed -i 's/^plugins=(.*/plugins=(aliases aws fzf ssh thefuck vi-mode)/' ~/.zshrc
+echo 'source ~/.zshrc.local' >> ~/.zshrc
+
+echo "Done! Start a new zsh session to use your config."
