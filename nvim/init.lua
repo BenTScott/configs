27842c6d1d962151efa8ef431e4de207a2c7b4ca
Ben Scott <btscott@amazon.com>
2026-06-19 12:55:26 +0000

    Fix treesitter config

--- nvim/init.lua
+++ nvim/init.lua
@@ -208,50 +208,48 @@ require('lazy').setup({
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'go', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'rust', 'tsx', 'typescript', 'vim', 'vimdoc' },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true, disable = { 'python' } },
-        incremental_selection = {
+    opts = {
+      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'go', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'rust', 'tsx', 'typescript', 'vim', 'vimdoc' },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true, disable = { 'python' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<M-space>',
+        },
+      },
+      textobjects = {
+        select = {
           enable = true,
+          lookahead = true,
           keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<M-space>',
+            ['aa'] = '@parameter.outer',
+            ['ia'] = '@parameter.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
           },
         },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = { [']m'] = '@function.outer', [']]'] = '@class.outer' },
-            goto_next_end = { [']M'] = '@function.outer', [']['] = '@class.outer' },
-            goto_previous_start = { ['[m'] = '@function.outer', ['[['] = '@class.outer' },
-            goto_previous_end = { ['[M'] = '@function.outer', ['[]'] = '@class.outer' },
-          },
-          swap = {
-            enable = true,
-            swap_next = { ['<leader>a'] = '@parameter.inner' },
-            swap_previous = { ['<leader>A'] = '@parameter.inner' },
-          },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = { [']m'] = '@function.outer', [']]'] = '@class.outer' },
+          goto_next_end = { [']M'] = '@function.outer', [']['] = '@class.outer' },
+          goto_previous_start = { ['[m'] = '@function.outer', ['[['] = '@class.outer' },
+          goto_previous_end = { ['[M'] = '@function.outer', ['[]'] = '@class.outer' },
         },
-      }
-    end,
+        swap = {
+          enable = true,
+          swap_next = { ['<leader>a'] = '@parameter.inner' },
+          swap_previous = { ['<leader>A'] = '@parameter.inner' },
+        },
+      },
+    },
   },
 
   'nvim-tree/nvim-tree.lua',
